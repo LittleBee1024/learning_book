61254939bae80340e198fa1e5930e95ddcfb511b
Ben <yuxiang660@163.com>
2022-07-29 14:31:14 +0800

    update doc

--- docs/booknotes/csapp/02/code/right_shift/main.cpp
+++ docs/booknotes/csapp/02/code/right_shift/main.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 #include <bitset>
 
+template<typename T>
+std::string to_bin_str(T x)
+{
+   std::bitset<sizeof(T) * 8> bin(x);
+   return bin.to_string();
+}
+
 void logic_right_shift()
 {
    printf("%s\n", __func__);
    uint8_t i = 0b10010000;
    uint8_t i2 = i >> 4;
-   std::bitset<8> b(i);
-   std::bitset<8> b2(i2);
-   std::cout << "\ti: 0b" << b
-      << ", (i >> 4): 0b" << b2
+   std::cout << "\ti: 0b" << to_bin_str(i)
+      << ", (i >> 4): 0b" << to_bin_str(i2)
       << std::endl;
 }
 
@@ -18,10 +23,14 @@ void arithmetic_right_shift()
    printf("%s\n", __func__);
    int8_t i = 0b10010000;
    int8_t i2 = i >> 4;
-   std::bitset<8> b(i);
-   std::bitset<8> b2(i2);
-   std::cout << "\ti: 0b" << b
-      << ", (i >> 4): 0b" << b2
+   std::cout << "\ti: 0b" << to_bin_str(i)
+      << ", (i >> 4): 0b" << to_bin_str(i2)
+      << std::endl;
+
+   int8_t j = 0b01010000;
+   int8_t j2 = j >> 4;
+   std::cout << "\ti: 0b" << to_bin_str(j)
+      << ", (i >> 4): 0b" << to_bin_str(j2)
       << std::endl;
 }
 
