0c3d8f674e564ef590e2e4c01d165b78b9c733c7
Ben <yuxiang660@163.com>
2022-08-18 18:51:46 +0800

    clean code

--- docs/booknotes/csapp/04/code/y86_64/_common/isa.h
+++ docs/booknotes/csapp/04/code/y86_64/_common/isa.h
@@ -116,3 +116,55 @@ typedef unsigned char cc_t;
 
 #define PACK_CC(z, s, o) (((z) << 2) | ((s) << 1) | ((o) << 0))
 #define DEFAULT_CC PACK_CC(1, 0, 0)
+
+namespace ISA
+{
+   /**
+    * @brief Y86-64 instruction structure
+    */
+   struct Instr
+   {
+      const char *name;
+      unsigned char code; /* Byte code for instruction+op */
+      int bytes;
+      INSTR_ARG arg1;
+      int arg1pos;
+      int arg1hi; /* 0/1 for register argument, # bytes for allocation */
+      INSTR_ARG arg2;
+      int arg2pos;
+      int arg2hi; /* 0/1 */
+   };
+
+   /**
+    * @brief Find Y86-64 instruction struction by the instruction name
+    * 
+    * @param name Instruction name
+    * @return const Instr&, return the reference of Y86-64 instruction struction.
+    *         If no instruction is found, return the empty instruction.
+    */
+   const Instr &findInstr(const char *name);
+
+   /**
+    * @brief Decode Y86-64 instruction name by the icode
+    * 
+    * @param code Instruction code number
+    * @return const char*, return the instruction name. For invalid icode, return nullptr.
+    */
+   const char *decodeInstrName(int code);
+
+   /**
+    * @brief Find Y86-64 reigster ID by register name
+    * 
+    * @param name Register name
+    * @return Y_REG_ID, for invalid register name, return Y_REG_ID::ERR
+    */
+   REG_ID findRegister(const char *name);
+
+   /**
+    * @brief Get the Y86-64 register name by register ID
+    * 
+    * @param id Register ID
+    * @return const char*, for invalid ID, return nullptr
+    */
+   const char *getRegName(REG_ID id);
+}
--- docs/booknotes/csapp/04/code/y86_64/assembler/yas_instr.cpp
+++ docs/booknotes/csapp/04/code/y86_64/assembler/yas_instr.cpp
@@ -3,88 +3,6 @@
 #include "isa.h"
 
 #include <string.h>
-#include <string>
-
-namespace
-{
-   struct Instr
-   {
-      const char *name;
-      unsigned char code; /* Byte code for instruction+op */
-      int bytes;
-      INSTR_ARG arg1;
-      int arg1pos;
-      int arg1hi; /* 0/1 for register argument, # bytes for allocation */
-      INSTR_ARG arg2;
-      int arg2pos;
-      int arg2hi; /* 0/1 */
-   };
-
-   const std::vector<Instr> Y86_64_INSTR = {
-       {"nop", HPACK(I_NOP, F_NONE), 1, NO_ARG, 0, 0, NO_ARG, 0, 0},
-       {"halt", HPACK(I_HALT, F_NONE), 1, NO_ARG, 0, 0, NO_ARG, 0, 0},
-       {"rrmovq", HPACK(I_RRMOVQ, F_NONE), 2, R_ARG, 1, 1, R_ARG, 1, 0},
-       /* Conditional move instructions are variants of RRMOVQ */
-       {"cmovle", HPACK(I_RRMOVQ, C_LE), 2, R_ARG, 1, 1, R_ARG, 1, 0},
-       {"cmovl", HPACK(I_RRMOVQ, C_L), 2, R_ARG, 1, 1, R_ARG, 1, 0},
-       {"cmove", HPACK(I_RRMOVQ, C_E), 2, R_ARG, 1, 1, R_ARG, 1, 0},
-       {"cmovne", HPACK(I_RRMOVQ, C_NE), 2, R_ARG, 1, 1, R_ARG, 1, 0},
-       {"cmovge", HPACK(I_RRMOVQ, C_GE), 2, R_ARG, 1, 1, R_ARG, 1, 0},
-       {"cmovg", HPACK(I_RRMOVQ, C_G), 2, R_ARG, 1, 1, R_ARG, 1, 0},
-       /* arg1hi indicates number of bytes */
-       {"irmovq", HPACK(I_IRMOVQ, F_NONE), 10, I_ARG, 2, 8, R_ARG, 1, 0},
-       {"rmmovq", HPACK(I_RMMOVQ, F_NONE), 10, R_ARG, 1, 1, M_ARG, 1, 0},
-       {"mrmovq", HPACK(I_MRMOVQ, F_NONE), 10, M_ARG, 1, 0, R_ARG, 1, 1},
-       {"addq", HPACK(I_ALU, A_ADD), 2, R_ARG, 1, 1, R_ARG, 1, 0},
-       {"subq", HPACK(I_ALU, A_SUB), 2, R_ARG, 1, 1, R_ARG, 1, 0},
-       {"andq", HPACK(I_ALU, A_AND), 2, R_ARG, 1, 1, R_ARG, 1, 0},
-       {"xorq", HPACK(I_ALU, A_XOR), 2, R_ARG, 1, 1, R_ARG, 1, 0},
-       /* arg1hi indicates number of bytes */
-       {"jmp", HPACK(I_JMP, C_YES), 9, I_ARG, 1, 8, NO_ARG, 0, 0},
-       {"jle", HPACK(I_JMP, C_LE), 9, I_ARG, 1, 8, NO_ARG, 0, 0},
-       {"jl", HPACK(I_JMP, C_L), 9, I_ARG, 1, 8, NO_ARG, 0, 0},
-       {"je", HPACK(I_JMP, C_E), 9, I_ARG, 1, 8, NO_ARG, 0, 0},
-       {"jne", HPACK(I_JMP, C_NE), 9, I_ARG, 1, 8, NO_ARG, 0, 0},
-       {"jge", HPACK(I_JMP, C_GE), 9, I_ARG, 1, 8, NO_ARG, 0, 0},
-       {"jg", HPACK(I_JMP, C_G), 9, I_ARG, 1, 8, NO_ARG, 0, 0},
-       {"call", HPACK(I_CALL, F_NONE), 9, I_ARG, 1, 8, NO_ARG, 0, 0},
-       {"ret", HPACK(I_RET, F_NONE), 1, NO_ARG, 0, 0, NO_ARG, 0, 0},
-       {"pushq", HPACK(I_PUSHQ, F_NONE), 2, R_ARG, 1, 1, NO_ARG, 0, 0},
-       {"popq", HPACK(I_POPQ, F_NONE), 2, R_ARG, 1, 1, NO_ARG, 0, 0},
-       {"iaddq", HPACK(I_IADDQ, F_NONE), 10, I_ARG, 2, 8, R_ARG, 1, 0},
-       /* this is just a hack to make the I_POP2 code have an associated name */
-       {"pop2", HPACK(I_POP2, F_NONE), 0, NO_ARG, 0, 0, NO_ARG, 0, 0},
-       /* For allocation instructions, arg1hi indicates number of bytes */
-       {".byte", 0x00, 1, I_ARG, 0, 1, NO_ARG, 0, 0},
-       {".word", 0x00, 2, I_ARG, 0, 2, NO_ARG, 0, 0},
-       {".long", 0x00, 4, I_ARG, 0, 4, NO_ARG, 0, 0},
-       {".quad", 0x00, 8, I_ARG, 0, 8, NO_ARG, 0, 0},
-       /* empty instruction */
-       {nullptr, 0, 0, NO_ARG, 0, 0, NO_ARG, 0, 0}};
-
-   const Instr &findInstr(const char *name)
-   {
-      for (const auto &instr : Y86_64_INSTR)
-      {
-         if (strcmp(instr.name, name) == 0)
-            return instr;
-      }
-      return Y86_64_INSTR.back(); // invalid instruction
-   }
-
-   const std::vector<std::string> Y86_64_REG_NAMES = {"%rax", "%rcx", "%rdx", "%rbx", "%rsp", "%rbp", "%rsi", "%rdi",
-                                                      "%r8", "%r9", "%r10", "%r11", "%r12", "%r13", "%r14"};
-
-   REG_ID findRegister(const char *name)
-   {
-      for (size_t i = 0; i < Y86_64_REG_NAMES.size(); i++)
-      {
-         if (Y86_64_REG_NAMES[i].compare(name) == 0)
-            return (REG_ID)i;
-      }
-      return REG_ERR;
-   }
-}
 
 namespace YAS
 {
@@ -97,7 +15,7 @@ namespace YAS
    {
       m_decodeBuf.clear();
 
-      const auto &instr = findInstr(name);
+      const auto &instr = ISA::findInstr(name);
       if (instr.name == nullptr)
       {
          m_lex.fail("Invalid Instruction");
@@ -178,7 +96,7 @@ namespace YAS
          return;
       }
 
-      rval = findRegister(m_lex.m_tokens.front().sval.c_str());
+      rval = ISA::findRegister(m_lex.m_tokens.front().sval.c_str());
       m_lex.m_tokens.pop_front();
 
       /* Insert into output */
@@ -231,7 +149,7 @@ namespace YAS
                return;
             }
 
-            rval = findRegister(m_lex.m_tokens.front().sval.c_str());
+            rval = ISA::findRegister(m_lex.m_tokens.front().sval.c_str());
             m_lex.m_tokens.pop_front();
             if (m_lex.m_tokens.front().type != TOK_PUNCT || m_lex.m_tokens.front().cval != ')')
             {
