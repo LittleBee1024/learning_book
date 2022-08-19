8159b5022b1805e5f49cc922c03650fb90af79fc
Ben <yuxiang660@163.com>
2022-08-19 22:17:20 +0800

    clean code

--- docs/booknotes/csapp/04/code/y86_64/simulator/sim_seq.h
+++ docs/booknotes/csapp/04/code/y86_64/simulator/sim_seq.h
@@ -24,16 +24,6 @@ namespace SIM
 
    private:
       word_t m_ftpc; // fall-through PC
-
-      struct PCInputs
-      {
-         byte_t icode = I_NOP;
-         word_t valc = 0;
-         word_t valm = 0;
-         word_t valp = 0;
-         bool bcond = false;
-      };
-      PCInputs m_pcInputs;
    };
 
 }
