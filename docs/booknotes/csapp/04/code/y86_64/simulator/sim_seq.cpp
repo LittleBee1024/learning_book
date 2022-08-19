e149f26f244723339c1c15f46c82a61436070143
Ben <yuxiang660@163.com>
2022-08-19 15:50:56 +0800

    add variable

--- docs/booknotes/csapp/04/code/y86_64/simulator/sim_seq.cpp
+++ docs/booknotes/csapp/04/code/y86_64/simulator/sim_seq.cpp
@@ -1,4 +1,41 @@
 #include "./sim_seq.h"
+#include "./seq.h"
+
+#include "isa.h"
+
+namespace SEQ
+{
+   /* Results computed by previous instruction. Used to compute PC in current instruction */
+   byte_t prev_icode = I_NOP;
+   byte_t prev_ifun = 0;
+   word_t prev_valc = 0;
+   word_t prev_valm = 0;
+   word_t prev_valp = 0;
+   bool prev_bcond = false;
+
+   /* Intermdiate stage values that must be used by control functions */
+   byte_t imem_icode = I_NOP;
+   byte_t imem_ifun = F_NONE;
+   byte_t icode = I_NOP;
+   word_t ifun = 0;
+   word_t ra = REG_NONE;
+   word_t rb = REG_NONE;
+   word_t valc = 0;
+   word_t valp = 0;
+   bool imem_error = false;
+   bool instr_valid = false;
+
+   word_t vala = 0;
+   word_t valb = 0;
+   word_t vale = 0;
+
+   bool bcond = false;
+   bool cond = false;
+   word_t valm = 0;
+   bool dmem_error = false;
+
+   byte_t status = SIM::STAT_OK;
+}
 
 namespace SIM
 {
