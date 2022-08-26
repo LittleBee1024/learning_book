80a5eb4008b597a97039b406c54f3d730e2e613b
Ben <yuxiang660@163.com>
2022-08-26 20:46:08 +0800

    update test

--- docs/topics/tcl_tk_in_c/code/tk/exec_log/main.tcl
+++ docs/topics/tcl_tk_in_c/code/tk/exec_log/main.tcl
@@ -7,36 +7,42 @@ wm title . ExecLog
 # Create a frame for buttons and entry.
 
 frame .top -borderwidth 10
+# The -fill x packing option makes the frame fill out to the whole width of the main window
 pack .top -side top -fill x
 
 # Create the command buttons.
 
 button .top.quit -text Quit -command exit
+# store .top.run button in "but" var
 set but [button .top.run -text "Run it" -command Run]
+# pack start from right
 pack .top.quit .top.run -side right
 
 # Create a labeled entry for the command
 
 label .top.l -text Command: -padx 0
-entry .top.cmd -width 20 -relief sunken \
-   -textvariable command
+entry .top.cmd -width 20 -relief sunken -textvariable command
 pack .top.l -side left
+# "-expand true" expands its packing space and fill up that extra area with its display
 pack .top.cmd -side left -fill x -expand true
 
 # Set up key binding equivalents to the buttons
 
 bind .top.cmd <Return> Run
 bind .top.cmd <Control-c> Stop
+# 开始程序后，主动聚焦在.top.cmd上，某则需要先用鼠标聚焦
 focus .top.cmd
 
 # Create a text widget to log the output
 
 frame .t
-set log [text .t.log -width 80 -height 10 \
+set log [listbox .t.log -width 80 -height 10 \
    -borderwidth 2 -relief raised -setgrid true \
-   -yscrollcommand {.t.scroll set}]
-scrollbar .t.scroll -command {.t.log yview}
-pack .t.scroll -side right -fill y
+   -yscrollcommand {.t.scrolly set} -xscrollcommand {.t.scrollx set}]
+scrollbar .t.scrolly -orient vertical -command {.t.log yview}
+scrollbar .t.scrollx -orient horizontal -command {.t.log xview}
+pack .t.scrolly -side right -fill y
+pack .t.scrollx -side bottom -fill x
 pack .t.log -side left -fill both -expand true
 pack .t -side top -fill both -expand true
 
@@ -45,10 +51,12 @@ pack .t -side top -fill both -expand true
 proc Run {} {
    global command input log but
    if [catch {open "|$command |& cat"} input] {
-      $log insert end $input\n
+      # print command error
+      $log insert end $input
    } else {
       fileevent $input readable Log
-      $log insert end $command\n
+      # print command line
+      $log insert end ">$command"
       $but config -text Stop -command Stop
    }
 }
@@ -61,8 +69,8 @@ proc Log {} {
       Stop
    } else {
       gets $input line
-      $log insert end $line\n
-      $log see end
+      # print command results
+      $log insert end $line
    }
 }
 
