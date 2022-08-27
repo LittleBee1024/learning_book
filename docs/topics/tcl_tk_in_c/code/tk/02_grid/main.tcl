2b4cf08e15ae28e33b60188c0d5fa4fdbeb7ab23
Ben <yuxiang660@163.com>
2022-08-27 20:52:38 +0800

    clean test

---
+++ docs/topics/tcl_tk_in_c/code/tk/02_grid/main.tcl
@@ -0,0 +1,23 @@
+foreach color {red orange yellow green blue purple} {
+   label .l1$color -text $color -bg white
+   frame .f1$color -background $color -width 100 -height 5
+   # 放置label和frame
+   grid .l1$color .f1$color
+   # 调整对齐方式：west
+   grid .l1$color -sticky w
+   # north south
+   grid .f1$color -sticky ns
+}
+
+label .lsplit -text --------
+frame .fsplit -width 100 -height 20
+grid .lsplit .fsplit
+
+foreach color {red orange yellow green blue purple} {
+   label .l2$color -text $color -bg white
+   frame .f2$color -background $color -width 100 -height 5
+   grid .l2$color .f2$color
+   # 外部和内部边框
+   grid .l2$color -column 1 -sticky w -padx 10 -ipady 5
+   grid .f2$color -column 0 -sticky ns -pady 5
+}
