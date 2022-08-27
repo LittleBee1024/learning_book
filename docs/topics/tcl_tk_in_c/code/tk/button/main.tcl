adfeba5d039f4c901cab476716c2323bd3ba8c70
Ben <yuxiang660@163.com>
2022-08-27 18:14:05 +0800

    update test

--- docs/topics/tcl_tk_in_c/code/tk/button/main.tcl
+++ docs/topics/tcl_tk_in_c/code/tk/button/main.tcl
@@ -1,19 +1,17 @@
 #!/usr/bin/wish
 
-# store widget instance .hello into a variable "hello_button"
+# 将按钮实例存于“$hello_button”，后面对按钮的操作都可以通过“$hello_button”完成
 set hello_button [button .hello -text "Hello, world!" -command {puts stdout "Hello, World!"}]
-
-# Use $hello_button as a command to oeprate on the button, so that the script doesn't
-# depends on the widget pathname
+# 配置按钮背景色
 $hello_button config -background green
-
+# 将按钮在屏幕上显示
 pack $hello_button -padx 20 -pady 10
 
-# get widget attribute
+# 获取按钮的属性
 set color [$hello_button cget -background]
 puts "hello buttton background: $color"
 
-# print out all the information about a widget
+# 通过函数打印组件的所有属性
 proc Widget_Attributes {w {out stdout}} {
    puts $out [format "%-20s %-10s %s" Attribute Default Value]
    foreach item [$w configure] {
