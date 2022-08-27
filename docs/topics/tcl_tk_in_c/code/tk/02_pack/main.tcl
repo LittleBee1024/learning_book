5d4ba17ce43e7e4691ef806f66beb7f756d44ada
Ben <yuxiang660@163.com>
2022-08-27 21:55:33 +0800

    add test

---
+++ docs/topics/tcl_tk_in_c/code/tk/02_pack/main.tcl
@@ -0,0 +1,36 @@
+# Chapter 23 The Pack Geometry Manager
+# Chapter 32-1 Frames, Labelframes, and Toplevel Windows
+
+# 普通布局
+. config -bg black
+frame .one -width 40 -height 40 -bg white
+labelframe .two -text split1 -width 200 -height 20 -bg grey50
+pack .one .two -side top
+
+
+# 存在子布局
+frame .three -bg white
+foreach b {alpha beta gamma} {
+   button .three.$b -text $b
+   pack .three.$b -side left
+}
+labelframe .four -text split2 -width 200 -height 20 -bg grey50
+pack .three .four -side top
+
+
+# 存在三层布局
+frame .five -bg white
+foreach b {alpha beta} {
+   button .five.$b -text $b
+   pack .five.$b -side left
+}
+# Create a frame for two more buttons
+frame .five.right
+foreach b {delta epsilon} {
+   button .five.right.$b -text $b
+   pack .five.right.$b -side bottom
+}
+labelframe .six -text split3 -width 200 -height 20 -bg grey50
+pack .five.right -side right
+pack .five .six -side top
+
