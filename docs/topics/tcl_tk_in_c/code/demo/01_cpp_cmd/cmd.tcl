55741de8e6aac8a4a368a1f09284d2f6ee91dba5
Ben <yuxiang660@163.com>
2022-08-29 14:27:38 +0800

    clean code

--- docs/topics/tcl_tk_in_c/code/demo/01_cpp_cmd/cmd.tcl
+++ docs/topics/tcl_tk_in_c/code/demo/01_cpp_cmd/cmd.tcl
@@ -1,3 +1,4 @@
+# 执行自定义的“blob”命令
 blob add Jack 100
 set res1 [blob find Jack]
 puts "\[  TCL\] Jack's age is ${res1}"
@@ -5,6 +6,6 @@ blob find Tim
 blob remove Jack
 blob find Jack
 blob remove Jack
-# invalid command, report error
+# 无效命令
 blob clear
 
