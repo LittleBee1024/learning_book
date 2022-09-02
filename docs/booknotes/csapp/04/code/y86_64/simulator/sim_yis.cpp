d214d1b67da390fa883ea9f7a8a49ce567fe7321
Ben <yuxiang660@163.com>
2022-09-02 13:50:42 +0800

    update test

--- docs/booknotes/csapp/04/code/y86_64/simulator/sim_yis.cpp
+++ docs/booknotes/csapp/04/code/y86_64/simulator/sim_yis.cpp
@@ -11,208 +11,276 @@ namespace SIM
    {
       m_out->out("\n[INFO] Cycle %d starts with CC='%s'\n", m_curCyc++, ISA::getCCName(m_cc));
 
-      word_t ftpc = m_pc; // fall-through PC
-      m_out->out("F: predPC = 0x%llx\n", ftpc);
+      m_stage.reset();
+
+      m_stage.f.valP = m_pc; // fall-through PC
+      m_out->out("F: predPC = 0x%llx\n", m_stage.f.valP);
 
       byte_t byte0 = 0;   // (icode+ifun)
-      if (!m_mem.getByte(ftpc, &byte0))
+      if (!m_mem.getByte(m_stage.f.valP, &byte0))
       {
          m_out->out("[ERROR] PC = 0x%llx, Invalid instruction address\n", m_pc);
          return STAT_ERR_ADDR;
       }
-      ftpc++;
+      m_stage.f.valP++;
 
-      INSTR icode = (INSTR)HI4(byte0);
-      I_FUN ifun = (I_FUN)LO4(byte0);
+      m_stage.f.icode = (INSTR)HI4(byte0);
+      m_stage.f.ifun = (I_FUN)LO4(byte0);
 
       byte_t byte1 = 0; // (rA+rB)
-      bool needReg = (icode == I_RRMOVQ || icode == I_ALU || icode == I_PUSHQ ||
-                      icode == I_POPQ || icode == I_IRMOVQ || icode == I_RMMOVQ ||
-                      icode == I_MRMOVQ || icode == I_IADDQ);
-      REG_ID rA = REG_NONE;
-      REG_ID rB = REG_NONE;
+      bool needReg = (m_stage.f.icode == I_RRMOVQ || m_stage.f.icode == I_ALU || m_stage.f.icode == I_PUSHQ ||
+                      m_stage.f.icode == I_POPQ || m_stage.f.icode == I_IRMOVQ || m_stage.f.icode == I_RMMOVQ ||
+                      m_stage.f.icode == I_MRMOVQ || m_stage.f.icode == I_IADDQ);
       if (needReg)
       {
-         if (!m_mem.getByte(ftpc, &byte1))
+         if (!m_mem.getByte(m_stage.f.valP, &byte1))
          {
             m_out->out("[ERROR] PC = 0x%llx, Invalid instruction address\n", m_pc);
             return STAT_ERR_ADDR;
          }
-         ftpc++;
+         m_stage.f.valP++;
 
-         if (icode == I_RRMOVQ || icode == I_ALU || icode == I_RMMOVQ || icode == I_MRMOVQ)
+         if (m_stage.f.icode == I_RRMOVQ || m_stage.f.icode == I_ALU ||
+             m_stage.f.icode == I_RMMOVQ || m_stage.f.icode == I_MRMOVQ)
          {
-            rA = (REG_ID)HI4(byte1);
-            rB = (REG_ID)LO4(byte1);
-            if (!VALID_REG(rA) || !VALID_REG(rB))
+            m_stage.f.rA = (REG_ID)HI4(byte1);
+            m_stage.f.rB = (REG_ID)LO4(byte1);
+            if (!VALID_REG(m_stage.f.rA) || !VALID_REG(m_stage.f.rB))
             {
-               m_out->out("[ERROR] PC = 0x%llx, Invalid registers rA=0x%.1x, rB=0x%.1x\n", m_pc, rA, rB);
+               m_out->out("[ERROR] PC = 0x%llx, Invalid registers rA=0x%.1x, rB=0x%.1x\n", m_pc, m_stage.f.rA, m_stage.f.rB);
                return STAT_ERR_REG;
             }
          }
 
-         if (icode == I_PUSHQ || icode == I_POPQ)
+         if (m_stage.f.icode == I_PUSHQ || m_stage.f.icode == I_POPQ)
          {
-            rA = (REG_ID)HI4(byte1);
-            if (!VALID_REG(rA))
+            m_stage.f.rA = (REG_ID)HI4(byte1);
+            if (!VALID_REG(m_stage.f.rA))
             {
-               m_out->out("[ERROR] PC = 0x%llx, Invalid register rA=0x%.1x\n", m_pc, rA);
+               m_out->out("[ERROR] PC = 0x%llx, Invalid register rA=0x%.1x\n", m_pc, m_stage.f.rA);
                return STAT_ERR_REG;
             }
          }
 
-         if (icode == I_IRMOVQ || icode == I_IADDQ)
+         if (m_stage.f.icode == I_IRMOVQ || m_stage.f.icode == I_IADDQ)
          {
-            rB = (REG_ID)LO4(byte1);
-            if (!VALID_REG(rB))
+            m_stage.f.rB = (REG_ID)LO4(byte1);
+            if (!VALID_REG(m_stage.f.rB))
             {
-               m_out->out("[ERROR] PC = 0x%llx, Invalid register rB=0x%.1x\n", m_pc, rB);
+               m_out->out("[ERROR] PC = 0x%llx, Invalid register rB=0x%.1x\n", m_pc, m_stage.f.rB);
                return STAT_ERR_REG;
             }
          }
       }
 
-      word_t cval = 0;
-      bool needImm = (icode == I_IRMOVQ || icode == I_RMMOVQ || icode == I_MRMOVQ ||
-                      icode == I_JMP || icode == I_CALL || icode == I_IADDQ);
+      bool needImm = (m_stage.f.icode == I_IRMOVQ || m_stage.f.icode == I_RMMOVQ || m_stage.f.icode == I_MRMOVQ ||
+                      m_stage.f.icode == I_JMP || m_stage.f.icode == I_CALL || m_stage.f.icode == I_IADDQ);
       if (needImm)
       {
-         if (!m_mem.getWord(ftpc, &cval))
+         if (!m_mem.getWord(m_stage.f.valP, &m_stage.f.valC))
          {
             m_out->out("[ERROR] PC = 0x%llx, Invalid instruction address\n", m_pc);
             return STAT_ERR_ADDR;
          }
-         ftpc += sizeof(word_t);
+
+         m_stage.f.valP += sizeof(word_t);
       }
+      m_out->out("D: instr = %s, rA = %s, rB = %s, valC = 0x%llx, valP = 0x%llx\n", ISA::decodeInstrName(HPACK(m_stage.f.icode, m_stage.f.ifun)),
+         ISA::getRegName((REG_ID)m_stage.f.rA), ISA::getRegName((REG_ID)m_stage.f.rB), m_stage.f.valC, m_stage.f.valP);
 
-      m_out->out("D: instr = %s, rA = %s, rB = %s, valC = 0x%llx, valP = 0x%llx\n", ISA::decodeInstrName(HPACK(icode, ifun)),
-         ISA::getRegName((REG_ID)rA), ISA::getRegName((REG_ID)rB), cval, ftpc);
-
-      switch (icode)
+      switch (m_stage.f.icode)
       {
       case I_HALT:
          return STAT_HLT;
       case I_NOP:
-         m_pc = ftpc;
+      {
+         m_stage.p.newPC = m_stage.f.valP;
          break;
+      }
       case I_RRMOVQ: // rrmovq rA, rB || cmovXX rA, rB
       {
-         word_t val = m_reg.getRegVal(rA);
-         if (checkCond(m_cc, (COND)ifun))
-            m_reg.setRegVal(rB, val);
-         m_pc = ftpc;
+         m_stage.d.srcA = m_stage.f.rA;
+         m_stage.d.valA = m_reg.getRegVal(m_stage.d.srcA);
+         m_stage.d.dstE = m_stage.f.rB;
+
+         m_stage.e.valE = m_stage.d.valA;
+
+         if ((m_stage.e.cnd = checkCond(m_cc, (COND)m_stage.f.ifun)) == true)
+            m_reg.setRegVal(m_stage.d.dstE, m_stage.e.valE);
+
+         m_stage.p.newPC = m_stage.f.valP;
          break;
       }
       case I_IRMOVQ: // irmovq V, rB
       {
-         m_reg.setRegVal(rB, cval);
-         m_pc = ftpc;
+         m_stage.d.dstE = m_stage.f.rB;
+
+         m_stage.e.valE = m_stage.f.valC;
+
+         m_reg.setRegVal(m_stage.d.dstE, m_stage.e.valE);
+
+         m_stage.p.newPC = m_stage.f.valP;
          break;
       }
       case I_RMMOVQ: // rmmovq rA, D(rB)
       {
-         cval += m_reg.getRegVal(rB);
-         word_t val = m_reg.getRegVal(rA);
-         if (m_mem.setWord(cval, val))
+         m_stage.d.srcA = m_stage.f.rA;
+         m_stage.d.srcB = m_stage.f.rB;
+         m_stage.d.valA = m_reg.getRegVal(m_stage.d.srcA);
+         m_stage.d.valB = m_reg.getRegVal(m_stage.d.srcB);
+
+         m_stage.e.valE = m_stage.d.valB + m_stage.f.valC;
+
+         if (m_mem.setWord(m_stage.e.valE, m_stage.d.valA))
          {
-            m_out->out("[ERROR] PC = 0x%llx, Invalid RMMOVQ instruction address: 0x%llx\n", m_pc, cval);
+            m_out->out("[ERROR] PC = 0x%llx, Invalid RMMOVQ instruction address: 0x%llx\n", m_pc, m_stage.e.valE);
             return STAT_ERR_ADDR;
          }
-         m_pc = ftpc;
+
+         m_stage.p.newPC = m_stage.f.valP;
          break;
       }
       case I_MRMOVQ: // mrmovq D(rB), rA
       {
-         cval += m_reg.getRegVal(rB);
-         word_t val = 0;
-         if (!m_mem.getWord(cval, &val))
+         m_stage.d.srcB = m_stage.f.rB;
+         m_stage.d.valB = m_reg.getRegVal(m_stage.d.srcB);
+         m_stage.d.dstM = m_stage.f.rA;
+
+         m_stage.e.valE = m_stage.d.valB + m_stage.f.valC;
+
+         if (!m_mem.getWord(m_stage.e.valE, &m_stage.m.valM))
          {
-            m_out->out("[ERROR] PC = 0x%llx, Invalid MRMOVQ instruction address: 0x%llx\n", m_pc, cval);
+            m_out->out("[ERROR] PC = 0x%llx, Invalid MRMOVQ instruction address: 0x%llx\n", m_pc, m_stage.e.valE);
             return STAT_ERR_ADDR;
          }
-         m_reg.setRegVal(rA, val);
-         m_pc = ftpc;
+
+         m_reg.setRegVal(m_stage.d.dstM, m_stage.m.valM);
+
+         m_stage.p.newPC = m_stage.f.valP;
          break;
       }
       case I_ALU: // OPq rA, rB
       {
-         word_t argA = m_reg.getRegVal(rA);
-         word_t argB = m_reg.getRegVal(rB);
-         word_t val = computeALU((ALU)ifun, argA, argB);
-         m_reg.setRegVal(rB, val);
-         m_cc = computeCC((ALU)ifun, argA, argB);
-         m_pc = ftpc;
+         m_stage.d.srcA = m_stage.f.rA;
+         m_stage.d.srcB = m_stage.f.rB;
+         m_stage.d.valA = m_reg.getRegVal(m_stage.d.srcA);
+         m_stage.d.valB = m_reg.getRegVal(m_stage.d.srcB);
+         m_stage.d.dstE = m_stage.f.rB;
+
+         m_stage.e.valE = computeALU((ALU)m_stage.f.ifun, m_stage.d.valA, m_stage.d.valB);
+         m_cc = computeCC((ALU)m_stage.f.ifun, m_stage.d.valA, m_stage.d.valB);
+
+         m_reg.setRegVal(m_stage.d.dstE, m_stage.e.valE);
+
+         m_stage.p.newPC = m_stage.f.valP;
          break;
       }
       case I_JMP: // jXX Dest
       {
-         if (checkCond(m_cc, (COND)ifun))
-            m_pc = cval;
+         if ((m_stage.e.cnd = checkCond(m_cc, (COND)m_stage.f.ifun)) == true)
+            m_stage.p.newPC = m_stage.f.valC;
          else
-            m_pc = ftpc;
+            m_stage.p.newPC = m_stage.f.valP;
          break;
       }
       case I_CALL: // call Dest
       {
-         word_t val = m_reg.getRegVal(REG_RSP) - sizeof(word_t);
-         m_reg.setRegVal(REG_RSP, val);
+         m_stage.d.srcB = REG_RSP;
+         m_stage.d.valB = m_reg.getRegVal(m_stage.d.srcB);
+         m_stage.d.dstE = REG_RSP;
+
+         m_stage.e.valE = m_stage.d.valB - sizeof(word_t);
+
          // push PC to stack
-         if (!m_mem.setWord(val, ftpc)) // write address to stack
+         if (!m_mem.setWord(m_stage.e.valE, m_stage.f.valP)) // write address to stack
          {
-            m_out->out("[ERROR] PC = 0x%llx, Invalid CALL instruction stack address: 0x%llx\n", m_pc, val);
+            m_out->out("[ERROR] PC = 0x%llx, Invalid instruction stack address: 0x%llx\n", m_pc, m_stage.e.valE);
             return STAT_ERR_ADDR;
          }
-         m_pc = cval;
+
+         m_reg.setRegVal(m_stage.d.dstE, m_stage.e.valE);
+
+         m_stage.p.newPC = m_stage.f.valC;
          break;
       }
       case I_RET: // ret
       {
+         m_stage.d.srcA = REG_RSP;
+         m_stage.d.srcB = REG_RSP;
+         m_stage.d.valA = m_reg.getRegVal(m_stage.d.srcA);
+         m_stage.d.valB = m_reg.getRegVal(m_stage.d.srcB);
+         m_stage.d.dstE = REG_RSP;
+
+         m_stage.e.valE = m_stage.d.valB + sizeof(word_t);
+
          // pop address from stack
-         word_t dval = m_reg.getRegVal(REG_RSP);
-         word_t val = 0;
-         if (!m_mem.getWord(dval, &val)) // read from stack
+         if (!m_mem.getWord(m_stage.d.valA, &m_stage.m.valM)) // read from stack
          {
-            m_out->out("[ERROR] PC = 0x%llx, Invalid RET instruction stack address: 0x%llx\n", m_pc, dval);
+            m_out->out("[ERROR] PC = 0x%llx, Invalid RET instruction stack address: 0x%llx\n", m_pc, m_stage.d.valA);
             return STAT_ERR_ADDR;
          }
-         m_reg.setRegVal(REG_RSP, dval + sizeof(word_t));
-         m_pc = val;
+
+         m_reg.setRegVal(m_stage.d.dstE, m_stage.e.valE);
+
+         m_stage.p.newPC = m_stage.m.valM;
          break;
       }
       case I_PUSHQ: // pushq rA
       {
-         word_t val = m_reg.getRegVal(rA);
-         word_t dval = m_reg.getRegVal(REG_RSP) - sizeof(word_t);
-         m_reg.setRegVal(REG_RSP, dval);
-         if (!m_mem.setWord(dval, val))
+         m_stage.d.srcA = m_stage.f.rA;
+         m_stage.d.srcB = REG_RSP;
+         m_stage.d.valA = m_reg.getRegVal(m_stage.d.srcA);
+         m_stage.d.valB = m_reg.getRegVal(m_stage.d.srcB);
+         m_stage.d.dstE = REG_RSP;
+
+         m_stage.e.valE = m_stage.d.valB - sizeof(word_t);
+
+         if (!m_mem.setWord(m_stage.e.valE, m_stage.d.valA))
          {
-            m_out->out("[ERROR] PC = 0x%llx, Invalid PUSHQ instruction stack address: 0x%llx\n", m_pc, dval);
+            m_out->out("[ERROR] PC = 0x%llx, Invalid PUSHQ instruction stack address: 0x%llx\n", m_pc, m_stage.e.valE);
             return STAT_ERR_ADDR;
          }
-         m_pc = ftpc;
+
+         m_reg.setRegVal(m_stage.d.dstE, m_stage.e.valE);
+
+         m_stage.p.newPC = m_stage.f.valP;
          break;
       }
       case I_POPQ: // popq rA
       {
-         word_t dval = m_reg.getRegVal(REG_RSP);
-         m_reg.setRegVal(REG_RSP, dval + sizeof(word_t));
-         word_t val = 0;
-         if (!m_mem.getWord(dval, &val))
+         m_stage.d.srcA = REG_RSP;
+         m_stage.d.srcB = REG_RSP;
+         m_stage.d.valA = m_reg.getRegVal(m_stage.d.srcA);
+         m_stage.d.valB = m_reg.getRegVal(m_stage.d.srcB);
+         m_stage.d.dstE = REG_RSP;
+         m_stage.d.dstM = m_stage.f.rA;
+
+         m_stage.e.valE = m_stage.d.valB + sizeof(word_t);
+
+         if (!m_mem.getWord(m_stage.d.valA, &m_stage.m.valM))
          {
-            m_out->out("[ERROR] PC = 0x%llx, Invalid POPQ instruction stack address: 0x%llx\n", m_pc, dval);
+            m_out->out("[ERROR] PC = 0x%llx, Invalid POPQ instruction stack address: 0x%llx\n", m_pc, m_stage.d.valA);
             return STAT_ERR_ADDR;
          }
-         m_reg.setRegVal(rA, val);
-         m_pc = ftpc;
+
+         m_reg.setRegVal(m_stage.d.dstE, m_stage.e.valE);
+         m_reg.setRegVal(m_stage.d.dstM, m_stage.m.valM);
+
+         m_stage.p.newPC = m_stage.f.valP;
          break;
       }
       case I_IADDQ:
       {
-         word_t argB = m_reg.getRegVal(rB);
-         word_t val = argB + cval;
-         m_reg.setRegVal(rB, val);
-         m_cc = computeCC(A_ADD, cval, argB);
-         m_pc = ftpc;
+         m_stage.d.srcB = m_stage.f.rB;
+         m_stage.d.valB = m_reg.getRegVal(m_stage.d.srcB);
+         m_stage.d.dstE = m_stage.f.rB;
+
+         m_stage.e.valE = m_stage.f.valC + m_stage.d.valB;
+         m_cc = computeCC(A_ADD, m_stage.f.valC, m_stage.d.valB);
+
+         m_reg.setRegVal(m_stage.d.dstE, m_stage.e.valE);
+
+         m_stage.p.newPC = m_stage.f.valP;
          break;
       }
       default:
@@ -221,6 +289,8 @@ namespace SIM
          return STAT_ERR_INSTR;
       }
       }
+
+      m_pc = m_stage.p.newPC;
       return STAT_OK;
    }
 }
